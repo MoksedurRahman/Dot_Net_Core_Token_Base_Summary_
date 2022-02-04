1120e21768a3de53cf8ebb1baeb13a1181889380
MoksedurRahman <moksedur.rahman@gmail.com>
2022-02-05 01:28:34 +0600

    initial

---
+++ SMECommerc.Databases/DbContexts/SMECommerceDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SMECommerce.Models.EntityModels;
+using SMECommerce.Models.EntityModels.Identity;
+
+namespace SMECommerce.Databases.DbContexts
+{
+    public class SMECommerceDbContext : IdentityDbContext<AppUser,AppRole,int>
+    {
+        public SMECommerceDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Products { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //string connectionString = @"Server=(local);Database=SMECommerceDB; Integrated Security=true";
+            //optionsBuilder
+            //    //.UseLazyLoadingProxies()
+            //    .UseSqlServer(connectionString);
+
+           
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<AppUser>(b => {
+
+                b.ToTable("AppUsers");
+
+
+                });
+        }
+    }
+}
---
+++ SMECommerce.App/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SMECommerce.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMECommerce.App.Controllers
+{
+    public class ProductController : Controller
+    {
+        IProductService _productService;
+        ICategoryService _categoryService; 
+        public ProductController(ICategoryService categoryService, IProductService productService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        [Authorize("AgeMinimum16")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
